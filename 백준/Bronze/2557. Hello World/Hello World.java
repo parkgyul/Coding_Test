b519601a5a676891af25ff657ed5b0cefe46e77f
Gyuri <98478441+parkgyul@users.noreply.github.com>
2024-11-27 15:42:55 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args)throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println("Hello World!");
+    }
+}

b48a8b68c8015e27cfa140648eb15edacc6f30f1
Gyuri <98478441+parkgyul@users.noreply.github.com>
2025-01-12 13:56:55 +0900

    [Silver IV] Title: 듣보잡, Time: 268 ms, Memory: 26436 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,35 +1,38 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.util.*;
 
-class Main{
-
-    public static void main(String[] args)throws IOException{
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        HashSet<String> notEverHeard = new HashSet<>();
-        ArrayList<String> notEverHeardSeen = new ArrayList<>();
+        Set<String> firstDepartment = new HashSet<>();
+        List<String> commonEmployees = new ArrayList<>();
 
-        for(int i = 0; i<n; i++){
-            notEverHeard.add(br.readLine());
-        }
-        for(int i = 0; i<m; i++){
-            String str = br.readLine();
-            if(notEverHeard.contains(str))
-                notEverHeardSeen.add(str);
+        for (int i = 0; i < n; i++) {
+            firstDepartment.add(br.readLine());
         }
 
-        Collections.sort(notEverHeardSeen);
+        for (int i = 0; i < m; i++) {
+            String employeeName = br.readLine();
+            // Check if the employee is in the first department
+            if (firstDepartment.contains(employeeName)) {
+                commonEmployees.add(employeeName);
+            }
+        }
 
-        System.out.println(notEverHeardSeen.size());
+        // Sort the result list
+        Collections.sort(commonEmployees);
 
-        for(int i = 0; i<notEverHeardSeen.size(); i++){
-            System.out.println(notEverHeardSeen.get(i));
+        // Print the result
+        System.out.println(commonEmployees.size());
+        for (String name : commonEmployees) {
+            System.out.println(name);
         }
     }
 }

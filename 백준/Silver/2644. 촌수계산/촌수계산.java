60a6f06cee2ada7e180dcd8d0b31744e1c2d8fe4
Gyuri <98478441+parkgyul@users.noreply.github.com>
2025-06-08 16:44:33 +0900

    [Silver II] Title: 촌수계산, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+
+import java.util.ArrayList;
+
+public class Main{
+    static int end, answer = -1;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        visited = new boolean[n+1];
+        for(int i = 0; i<= n; i++) graph.add(new ArrayList<>());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+
+        int m = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i< m; i++){
+            st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+
+            graph.get(parent).add(child);
+            graph.get(child).add(parent);
+        }
+
+        dfs(start, 0);
+        System.out.println(answer);
+
+
+    }
+    private static void dfs(int point, int cnt){
+        visited[point] = true;
+        for(int i : graph.get(point)){
+            if(!visited[i]){
+                if(i == end){
+                    answer = cnt + 1;
+                    return;
+                }
+                dfs(i, cnt+1);
+            }
+        }
+    }
+}

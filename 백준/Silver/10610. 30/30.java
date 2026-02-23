b4e048680a357c199127eecff39df3d7b7561fe3
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-02-23 17:22:19 +0900

    [Silver IV] Title: 30, Time: 148 ms, Memory: 16580 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,28 +1,31 @@
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.BufferedReader;
-
 import java.util.Arrays;
 
 class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        int sum = 0;
 
+        // String -> char Array
         char[] arr = str.toCharArray();
-
+        
         Arrays.sort(arr);
 
+        int sum = 0;
+
+        // 마지막 결과를 출력할 StringBuilder
         StringBuilder sb = new StringBuilder();
 
         for(int i = arr.length-1; i >= 0; i--){
-            int num = arr[i] - '0';
-            sum += num;
-            sb.append(num);
+            sum += arr[i] - '0';
+            sb.append(arr[i]);
         }
 
-        if(arr[0] != '0' || sum % 3 != 0){
+        // 조건 1. 끝자리가 0으로 끝나야함.
+        // 조건 2. 모든 수의 합이 3의 배수가 되어야 함.
+        if( arr[0] != '0' || sum % 3 != 0){
             System.out.print(-1);
             return;
         }

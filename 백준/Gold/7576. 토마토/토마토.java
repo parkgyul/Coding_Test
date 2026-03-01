7f3fb23ce8a507d051c6518d9bb25a779abcc8f6
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-03-02 00:32:47 +0900

    [Gold V] Title: 토마토, Time: 528 ms, Memory: 120072 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,75 +1,72 @@
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.util.StringTokenizer;
 import java.io.InputStreamReader;
-import java.util.Queue;
-import java.util.LinkedList;
-public class Main{
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
+import java.io.BufferedReader;
+import java.util.*;
+
+class Main{
+    static int n, m;
+    static int tomato, ripen_tomato;
+    static int[][] arr;
+    static Queue<Point> q = new LinkedList<>();
+    static int max = 0, cnt = 0;
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, 1, -1, 0};
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        //가로
-        int m = Integer.parseInt(st.nextToken());
-        //세로
-        int n = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n][m];
-        int tomato = 0;
-        int ripen_tomato = 0;
-        Queue<Point> q = new LinkedList<>();
+        m = Integer.parseInt(st.nextToken()); // 가로
+        n = Integer.parseInt(st.nextToken()); // 세로
 
-        for(int i = 0; i<n; i++){
-            st =  new StringTokenizer(br.readLine());
-            for(int j = 0; j<m; j++){
-                int num = Integer.parseInt(st.nextToken());
-                arr[i][j] = num;
-                if(num == 1) {
-                    q.add(new Point(j, i, 0));
+        arr = new int[n][m];
+        tomato = 0; // 되어야하는 토마토 수
+        ripen_tomato = 0;
+
+        for(int i =0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] != -1) tomato ++;
+                if(arr[i][j] == 1){
+                    q.add(new Point(i, j, 0));
                 }
-                if(num != -1) {
-                    tomato ++;
-                }
-             }
+            }
         }
-
 
         while(!q.isEmpty()){
             Point current = q.poll();
-            int current_x = current.x;
-            int current_y = current.y;
-            ripen_tomato ++;
-            if(tomato == ripen_tomato) {
-                System.out.println(current.cnt);
+            ripen_tomato++;
+            if(tomato == ripen_tomato){
+                System.out.println(current.t);
                 return;
             }
-            for(int i = 0; i<4; i++){
-                int next_x = current_x+dx[i];
-                int next_y = current_y+dy[i];
 
-                if(next_x<0 || next_x>=m || next_y<0 || next_y>=n)
+            for(int i = 0; i < 4; i++){
+                int next_i = current.i + dx[i];
+                int next_j = current.j + dy[i];
+
+                if(next_i <0 || next_j <0 || next_i >= n || next_j >= m)
                     continue;
-                if(arr[next_y][next_x]== -1 || arr[next_y][next_x] == 1)
+                if(arr[next_i][next_j] == -1 || arr[next_i][next_j] == 1)
                     continue;
 
-                q.add(new Point(next_x, next_y, current.cnt+1));
-                arr[next_y][next_x] = 1;
+                q.add(new Point(next_i, next_j, current.t+1));
+                arr[next_i][next_j] = 1; // 토마토 익히기
             }
-
         }
 
-        System.out.println(-1);
+        System.out.print(-1);
     }
+
     public static class Point{
-        private int x;
-        private int y;
-        private int cnt;
+        int i, j, t;
 
-        public Point(int x, int y, int cnt){
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
+        public Point(int i, int j, int t){
+            this.i = i;
+            this.j = j;
+            this.t = t;
         }
     }
+
 }

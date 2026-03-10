a353de8bbd05aebc0341ece359727027fead71f6
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-03-10 12:57:47 +0900

    [Silver II] Title: 아기 상어 2, Time: 116 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -14,46 +14,40 @@ public class Main{
         int m = Integer.parseInt(st.nextToken());
 
         int[][] arr = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        Queue<Point> q = new LinkedList<>();
+        int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0};
+        int[] dy = {-1, 0, 1, 1, 1, 0, -1, -1};
 
         for(int i = 0; i<n; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j<m; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] == 1){
+                    q.add(new Point(i, j, 0));
+                    visited[i][j] = true;
+                }
             }
         }
 
-
-        int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0};
-        int[] dy = {-1, 0, 1, 1, 1, 0, -1, -1};
-        boolean[][] visited;
-
-        int max = Integer.MIN_VALUE;
+        int max = 0;
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
-                visited = new boolean[n][m];
-                Queue<Point> q = new LinkedList<>();
-                if(arr[i][j] == 0){
-                    q.add(new Point(i, j, 0));
-                    while(!q.isEmpty()){
-                        Point current = q.poll();
-                        if(arr[current.i][current.j] == 1){
-                            max = Math.max(max, current.cnt);
-                            break;
-                        }
+                while(!q.isEmpty()){
+                    Point current = q.poll();
+                    max = current.cnt;
 
-                        for(int a = 0; a <8; a++){
-                            int next_i = current.i + dx[a];
-                            int next_j = current.j + dy[a];
+                    for(int a = 0; a <8; a++){
+                        int next_i = current.i + dx[a];
+                        int next_j = current.j + dy[a];
 
-                            if(next_i < 0 || next_j < 0 || next_i >=n || next_j >= m)
-                                continue;
-                            if(visited[next_i][next_j])
-                                continue;
+                        if(next_i < 0 || next_j < 0 || next_i >=n || next_j >= m)
+                            continue;
+                        if(visited[next_i][next_j])
+                            continue;
 
-                            q.add(new Point(next_i, next_j, current.cnt+1));
-                            visited[next_i][next_j] = true;
-                        }
-
+                        q.add(new Point(next_i, next_j, current.cnt+1));
+                        visited[next_i][next_j] = true;
                     }
                 }
             }

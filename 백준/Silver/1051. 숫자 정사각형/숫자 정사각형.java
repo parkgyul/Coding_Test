1da1a49e2adcd8217badf335ac795682c760b0d1
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-03-11 12:47:05 +0900

    [Silver III] Title: 숫자 정사각형, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][m];
+
+        for(int i = 0; i < n; i++){
+            String str = br.readLine();
+            for(int j = 0; j<m; j++){
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        int max = 1;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                int num = arr[i][j];
+                for(int k = 0; i+k < n && j+k < m; k++){
+                    if(num != arr[i+k][j]) continue;
+                    if(num != arr[i+k][j+k]) continue;
+                    if(num != arr[i][j+k]) continue;
+
+                    max = (int) Math.max(Math.pow(k+1, 2), max);
+                }
+            }
+        }
+
+        System.out.print(max);
+    }
+}

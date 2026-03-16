117907ae1aa55f0d408f3f95c5262de868d51291
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-03-16 17:43:24 +0900

    [Silver I] Title: 지름길, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -1,62 +1,60 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int D = Integer.parseInt(st.nextToken());
 
-        ArrayList<Node> nodes = new ArrayList<>();
-        for(int i = 0; i < N; i++){
+        List<Node>[] nodes = new ArrayList[D+1];
+
+        for(int i = 0; i <= D; i++){
+            nodes[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < N; i ++){
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int distance = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            if(end-start <= distance) continue;
             if(end > D) continue;
 
-            nodes.add(new Node(start, end, distance));
+            nodes[start].add(new Node(end, cost));
         }
 
-        nodes.sort((a, b) -> a.start - b.start);
+        int[] costs = new int[D+1];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        costs[0] = 0;
 
-        int[] dp = new int[D+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        for(int i = 0; i <= D-1; i++){
+            costs[i+1] = Math.min(costs[i+1], costs[i] + 1);
 
-        dp[0] = 0;
-        int index = 0;
-        for(int i = 0; i < D; i++){
-            // 일반 도로 이동
-            dp[i+1] = Math.min(dp[i+1], dp[i]+1);
+            for(Node node : nodes[i]){
+                int next = node.end;
+                int next_cost = node.cost;
 
-            // 지름길 처리
-            while(index < nodes.size() && nodes.get(index).start == i) {
-                Node current = nodes.get(index);
-                dp[current.end] = Math.min(dp[current.end], dp[current.start] + current.distance);
-                index++;
+                if(next_cost + costs[i] < costs[next]) {
+                    costs[next] = next_cost + costs[i];
+                }
             }
+
         }
 
 
-        System.out.print(dp[D]);
+        System.out.print(costs[D]);
     }
 
     public static class Node{
-        int start, end, distance;
-
-        public Node(int start, int end, int distance){
-            this.start = start;
+        int end, cost;
+        public Node(int end, int cost){
             this.end = end;
-            this.distance = distance;
+            this.cost = cost;
         }
     }
 }

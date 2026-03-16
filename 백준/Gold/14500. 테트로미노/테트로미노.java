c4f2d7a7ff4fbb765f24868fc19ba5885f43d117
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-03-16 13:26:10 +0900

    [Gold IV] Title: 테트로미노, Time: 636 ms, Memory: 31052 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,88 +1,69 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class Main{
+public class Main {
+
     static int N, M;
-    static int[][] arr;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    static int max;
+    static int[][] map;
     static boolean[][] visited;
+    static int max;
 
-    public static void main(String[] args)throws IOException{
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N][M];
+        map = new int[N][M];
+        visited = new boolean[N][M];
 
-        for(int i = 0; i < N; i++){
+        for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j <M; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0;j<M;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        visited = new boolean[N][M];
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j <M; j++){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
                 visited[i][j] = true;
-                dfs(i,j, 1, arr[i][j]);
-                checkT(i,j);
+                dfs(i,j,1,map[i][j]);
                 visited[i][j] = false;
             }
         }
 
-        System.out.print(max);
+        System.out.println(max);
     }
 
-    public static void dfs(int i, int j, int depth, int total){
+    static void dfs(int x, int y, int depth, int sum){
 
         if(depth == 4){
-            max = Math.max(total, max);
+            max = Math.max(max, sum);
             return;
         }
 
-        for(int a = 0; a < 4; a++){
-            int next_i = i + dx[a];
-            int next_j = j + dy[a];
+        for(int d=0; d<4; d++){
 
-            if(next_i < 0 || next_j < 0 || next_i >= N || next_j >= M || visited[next_i][next_j])
-                continue;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
 
-            visited[next_i][next_j] = true;
-            dfs(next_i, next_j, depth+1, total + arr[next_i][next_j]);
-            visited[next_i][next_j] = false;
-        }
-    }
+            if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
+            if(visited[nx][ny]) continue;
 
-    public static void checkT(int i, int j){
-        int min = Integer.MAX_VALUE;
-        int sum = arr[i][j];
-        int cnt = 1;
-        for(int a = 0; a< 4; a++){
-            int next_i = i + dx[a];
-            int next_j = j + dy[a];
+            visited[nx][ny] = true;
+            
+            if(depth == 2){
+                dfs(x, y, depth+1, sum + map[nx][ny]);
+            }
 
-            if(next_i < 0 || next_j < 0 || next_i >= N || next_j >= M )
-                continue;
+            dfs(nx, ny, depth+1, sum + map[nx][ny]);
 
-            min = Math.min(min, arr[next_i][next_j]);
-            sum += arr[next_i][next_j];
-            cnt ++;
-        }
-
-        if(cnt == 4){
-            max = Math.max(max, sum);
-        }else{
-            max = Math.max(max, sum - min);
+            visited[nx][ny] = false;
         }
     }
 }

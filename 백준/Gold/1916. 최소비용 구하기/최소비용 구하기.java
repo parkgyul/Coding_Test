adc51fa9dd840c6e284d9e40355d25ab4ffd4530
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-03-19 22:46:50 +0900

    [Gold V] Title: 최소비용 구하기, Time: 500 ms, Memory: 50644 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,79 +1,86 @@
-import java.io.IOException;
-import java.util.*;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.*;
 
-public class Main{
+class Main{
+    static int N, M;
+    static List<Node>[] nodes;
+    static int dest;
+    static int[] costs;
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine()); // 도시의 개수
+        M = Integer.parseInt(br.readLine()); // 버스의 개수
 
-        int N = Integer.parseInt(br.readLine()); // 도시 개수
-        int M = Integer.parseInt(br.readLine()); // 버스 개수
+        nodes = new ArrayList[N+1];
 
-        List<Node>[] graph = new ArrayList[N+1];
-
-        for(int i = 1; i <= N; i++){
-            graph[i] = new ArrayList<>();
+        for(int i = 1; i < N+1; i++){
+            nodes[i] = new ArrayList<>();
         }
 
         StringTokenizer st;
-
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            graph[start].add(new Node(end, cost));
+            nodes[s].add(new Node(e, c));
         }
-
 
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
+        dest = Integer.parseInt(st.nextToken());
 
-        int[] costs = new int[N+1];
+        costs = new int[N+1];
         Arrays.fill(costs, Integer.MAX_VALUE);
-        
+        bfs(start);
+
+        System.out.print(costs[dest]);
+    }
+    public static void bfs(int start){
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        
-        costs[start] = 0;
         pq.add(new Node(start, 0));
+        costs[start] = 0;
 
-       
+        boolean[] visited = new boolean[N+1];
+
         while(!pq.isEmpty()){
-            Node current = pq.poll();
-            int city = current.city;
-            int cost = current.cost;
+            Node cur = pq.poll();
+            int cur_city = cur.city;
+            int cur_cost = cur.cost;
 
-            if(cost > costs[city]) continue;
+            if(visited[cur_city])
+                continue;
 
-            for(Node next : graph[city]){
-                int nextCity = next.city;
-                int nextCost = cost + next.cost;
-                
-                if(costs[nextCity] > nextCost){
-                    costs[nextCity] = nextCost;
-                    pq.add(new Node(nextCity, nextCost));
-                    
-                }
+            visited[cur_city] = true;
+
+            for(Node next : nodes[cur_city]){
+                int next_city = next.city;
+
+                int next_cost = cur_cost + next.cost;
+
+                if(next_cost >= costs[next_city])
+                    continue;
+
+                costs[next_city] = next_cost;
+                pq.add(new Node(next_city, next_cost));
             }
         }
 
-        System.out.print(costs[end]);
-
     }
 
-    static class Node implements Comparable<Node>{
+    public static class Node implements Comparable<Node>{
         int city, cost;
-        Node(int city, int cost){
+        public Node(int city, int cost){
             this.city = city;
             this.cost = cost;
         }
 
-        public int compareTo(Node o) {
+        public int compareTo(Node o){
             return this.cost - o.cost;
         }
+
     }
 }

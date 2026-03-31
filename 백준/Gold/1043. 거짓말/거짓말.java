38c0800ba00d42063622a33c0744eb678cf4210b
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-03-31 13:11:35 +0900

    [Gold IV] Title: 거짓말, Time: 108 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,58 +1,50 @@
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.StringTokenizer;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
 
 public class Main{
     static int[] parents;
-
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 사람 수
-        int M = Integer.parseInt(st.nextToken()); // 파티 수
-
-
-        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         parents = new int[N+1];
         for(int i = 1; i < N+1; i++){
             parents[i] = i;
         }
 
-
+        st = new StringTokenizer(br.readLine());
         int truthCount = Integer.parseInt(st.nextToken());
         int[] truth = new int[truthCount];
-        for(int i = 0; i < truthCount; i++){
+
+        for(int i =0; i < truthCount; i++){
             truth[i] = Integer.parseInt(st.nextToken());
         }
 
         List<List<Integer>> parties = new ArrayList<>();
-        for(int i = 0; i < M; i++){ // 파티 수
+        for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int cnt = Integer.parseInt(st.nextToken());
-
+            int num = Integer.parseInt(st.nextToken());
             List<Integer> party = new ArrayList<>();
-            for(int j = 0; j < cnt; j++){
-                party.add(Integer.parseInt(st.nextToken()));
+            for(int j = 0; j < num; j++){
+                int person = Integer.parseInt(st.nextToken());
+                party.add(person);
             }
 
+            for(int j = 0; j < party.size() -1; j++){
+                union(party.get(j), party.get(j+1));
+            }
             parties.add(party);
-
-            // 같은 파티는 union
-            for(int j = 0 ; j < party.size() - 1; j++){
-                union(party.get(j), party.get(j + 1));
-            }
         }
 
         int result = 0;
-
         for(List<Integer> party : parties){
             boolean canLie = true;
 
@@ -66,19 +58,21 @@ public class Main{
                 if(!canLie) break;
             }
 
-            if(canLie) result++;
+            if(canLie) result ++;
         }
 
-        System.out.println(result);
+        System.out.print(result);
     }
-    static int find(int x){
-        if(parents[x] == x) return x;
-        return parents[x] = find(parents[x]);
+
+    static int find(int a){
+        if(parents[a] == a) return a;
+        return parents[a] = find(parents[a]);
     }
 
     static void union(int a, int b){
         int pa = find(a);
         int pb = find(b);
+
         if(pa != pb) parents[pb] = pa;
     }
 }

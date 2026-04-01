e9c18bacd753f679e54842f25426bf9e00a8f399
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-04-01 17:16:25 +0900

    [Gold V] Title: 암호 만들기, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,71 +1,56 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
     static int L, C;
     static List<Character> c;
     static StringBuilder sb = new StringBuilder();
     static char[] result;
-    public static void main(String[] args)throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         c = new ArrayList<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < C; i++){
-            char ch = st.nextToken().charAt(0);
-
-            c.add(ch);
+        for (int i = 0; i < C; i++) {
+            c.add(st.nextToken().charAt(0));
         }
 
         Collections.sort(c);
+        result = new char[L];
 
-        for(int i = 0; i < C-L+1; i++){
-            result = new char[L];
-            int cnt = 0;
-            char ch = c.get(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                cnt++;
-            }
-            result[0] = ch;
-            dfs(1, i, cnt);
-        }
+        dfs(0, 0, 0);
 
         System.out.print(sb);
     }
 
-    public static void dfs(int depth, int index, int moCnt){
-        if(depth == L){
-            if(moCnt >= 1 && L - moCnt >= 2){
-                for(int i = 0; i < L; i++){
-                    sb.append(result[i]);
-                }
-                sb.append("\n");
+    static void dfs(int depth, int start, int moCnt) {
+        if (depth == L) {
+            if (moCnt >= 1 && (L - moCnt) >= 2) {
+                sb.append(result).append("\n");
             }
             return;
         }
 
-        for(int i = index+1; i < C; i++){
+        for (int i = start; i < C; i++) {
             char ch = c.get(i);
-            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                if(L - moCnt < 2)
-                    continue;
 
-                result[depth] = ch;
-                dfs(depth+1, i, moCnt+1);
-                result[depth] = ' ';
-            }else{
-                result[depth] = ch;
-                dfs(depth+1, i, moCnt);
-                result[depth] = ' ';
+            result[depth] = ch;
+
+            if (isVowel(ch)) {
+                dfs(depth + 1, i + 1, moCnt + 1);
+            } else {
+                dfs(depth + 1, i + 1, moCnt);
             }
         }
+    }
+
+    static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 }

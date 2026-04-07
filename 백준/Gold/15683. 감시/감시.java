2c393e712ceb30605eea3fd423d2cd3a2a1abff1
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-04-07 15:25:37 +0900

    [Gold III] Title: 감시, Time: 204 ms, Memory: 35000 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,16 +1,14 @@
 import java.io.BufferedReader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main{
     static int N, M;
     static int[][] arr;
-    static int[][] cover;
-    static int blindSpot = 0;
-    static int minBlind = Integer.MAX_VALUE;
-    static List<CCTV> cctvs = new ArrayList<>();
-
-    static int[] dx = {-1, 0, 1, 0}; // 상, 우, 하, 좌
+    static int[][] covered;
+    static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int[][][] dirs = {
             {},
@@ -20,25 +18,27 @@ public class Main{
             {{0,1,2}, {1,2,3}, {2,3,0}, {3,0,1}},
             {{0,1,2,3}}
     };
-
+    static List<CCTV> cctvs;
+    static int blindSpot = 0;
+    static int minBlind = Integer.MAX_VALUE;
     public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
         arr = new int[N][M];
-        cover = new int[N][M];
+        covered = new int[N][M];
+        cctvs = new ArrayList<>();
 
-        for(int i = 0; i < N; i++){
+        for(int i =0 ; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
 
                 if(arr[i][j] == 0){
-                    blindSpot++;
+                    blindSpot ++;
                 }else if(1 <= arr[i][j] && arr[i][j] <= 5){
                     cctvs.add(new CCTV(i, j, arr[i][j]));
                 }
@@ -46,65 +46,61 @@ public class Main{
         }
 
         dfs(0);
-        System.out.println(minBlind);
+        System.out.print(minBlind);
     }
-
     public static void dfs(int index){
 
         if(index == cctvs.size()){
-            minBlind = Math.min(blindSpot, minBlind);
+            minBlind = Math.min(minBlind, blindSpot);
             return;
         }
 
-        CCTV cur = cctvs.get(index);
+        CCTV cctv = cctvs.get(index);
 
-        for(int[] dirSet : dirs[cur.type]){
-            List<Point> changed = watch(cur.i, cur.j, dirSet);
+        for(int[] d : dirs[cctv.type]){
+            List<Point> w = watch(d, cctv);
             dfs(index+1);
-            rollback(changed);
+            rollback(w);
         }
     }
 
-    static List<Point> watch(int i, int j, int[] dirSet){
-        List<Point> changed = new ArrayList<>();
+    public static List<Point> watch(int[] dirs, CCTV cctv){
+        List<Point> w = new ArrayList<>();
 
-        for(int d : dirSet){
-            int ni = i + dx[d];
-            int nj = j + dy[d];
-
+        for(int d : dirs){
+            int ni = cctv.i + dx[d];
+            int nj = cctv.j + dy[d];
             while(isIn(ni, nj) && arr[ni][nj] != 6){
                 if(arr[ni][nj] == 0){
-                    if(cover[ni][nj] == 0){
-                        blindSpot --;
+                    if(covered[ni][nj] == 0){
+                        blindSpot--;
                     }
-
-                    changed.add(new Point(ni, nj));
-                    cover[ni][nj] ++;
+                    covered[ni][nj] ++;
+                    w.add(new Point(ni, nj));
                 }
+
                 ni += dx[d];
                 nj += dy[d];
             }
         }
 
-        return changed;
+        return w;
     }
 
-    static void rollback(List<Point> changed){
-        for(Point p : changed){
-            cover[p.i][p.j]--;
-            if(cover[p.i][p.j] == 0){
-                blindSpot++;
+    public static void rollback(List<Point> w){
+        for(Point p : w){
+            covered[p.i][p.j] --;
+            if(covered[p.i][p.j] == 0){
+                blindSpot ++;
             }
         }
     }
 
-    static boolean isIn(int i, int j){
+    public static boolean isIn(int i, int j){
         return i >= 0 && i < N && j >= 0 && j < M;
     }
-
     public static class Point{
         int i, j;
-
         public Point(int i, int j){
             this.i = i;
             this.j = j;

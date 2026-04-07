5cbc03b201ae6bcf3a6c0d485b99b1719e782433
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-04-07 17:44:07 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -15,7 +15,6 @@ public class Main{
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
@@ -23,18 +22,15 @@ public class Main{
         visited = new boolean[N][M][N][M];
 
         int startRi = 0, startRj = 0, startBi = 0, startBj = 0;
-
         for(int i =0 ; i < N; i++){
             String str = br.readLine();
-            for(int j =0 ; j < M; j++){
-                char ch = str.charAt(j);
-                board[i][j] = ch;
-
-                if(ch == 'R'){
+            for(int j = 0; j < M; j++){
+                board[i][j] = str.charAt(j);
+                if(board[i][j] == 'R'){
                     startRi = i;
                     startRj = j;
                     board[i][j] = '.';
-                }else if(ch == 'B'){
+                }else if(board[i][j] == 'B'){
                     startBi = i;
                     startBj = j;
                     board[i][j] = '.';
@@ -46,51 +42,55 @@ public class Main{
     }
 
     public static int bfs(int startRi, int startRj, int startBi, int startBj){
-        Queue<State> q= new LinkedList<>();
+        Queue<State> q = new LinkedList<>();
         q.add(new State(startRi, startRj, startBi, startBj, 0));
         visited[startRi][startRj][startBi][startBj] = true;
-
         while(!q.isEmpty()){
             State cur = q.poll();
 
-            if(cur.depth >= 10) continue;
+            if(cur.time >= 10)
+                continue;
 
-            for(int dir = 0; dir < 4; dir++){
-                MoveResult red = move(cur.ri, cur.rj, dir);
-                MoveResult blue = move(cur.bi, cur.bj, dir);
+            for(int i = 0; i < 4; i++) {
+                moveResult red = move(cur.ri, cur.rj, i);
+                moveResult blue = move(cur.bi, cur.bj, i);
 
-                if(blue.inHole) continue;
-                if(red.inHole) return cur.depth + 1;
+                if (blue.inHole)
+                    continue;
+
+                if (red.inHole)
+                    return cur.time + 1;
 
                 int nri = red.i;
                 int nrj = red.j;
                 int nbi = blue.i;
                 int nbj = blue.j;
 
-                if(nri == nbi && nrj == nbj){
-                    if(red.count > blue.count){
-                        nri -= dx[dir];
-                        nrj -= dy[dir];
-                    }else{
-                        nbi -= dx[dir];
-                        nbj -= dy[dir];
+                if (nri == nbi && nrj == nbj) {
+                    if (red.count > blue.count) {
+                        nri -= dx[i];
+                        nrj -= dy[i];
+                    } else {
+                        nbi -= dx[i];
+                        nbj -= dy[i];
                     }
                 }
 
-                if(!visited[nri][nrj][nbi][nbj]){
+                if (!visited[nri][nrj][nbi][nbj]) {
                     visited[nri][nrj][nbi][nbj] = true;
-                    q.add(new State(nri, nrj, nbi, nbj, cur.depth+1));
+                    q.add(new State(nri, nrj, nbi, nbj, cur.time+1));
                 }
             }
+
         }
+
         return -1;
     }
 
-    public static MoveResult move(int i, int j, int dir){
+    public static moveResult move(int i, int j, int dir){
+        int count= 0;
         int ni = i;
         int nj = j;
-        int count = 0;
-
         while(true){
             int ti = ni + dx[dir];
             int tj = nj + dy[dir];
@@ -101,35 +101,34 @@ public class Main{
             nj = tj;
             count++;
 
-            if(board[ni][nj] == 'O'){
-                return new MoveResult(ni, nj, count, true);
+            if(board[ni][nj] == 'O') {
+                return new moveResult(ni, nj, count, true);
             }
         }
 
-        return new MoveResult(ni, nj, count, false);
+        return new moveResult(ni, nj, count, false);
     }
 
-    public static class MoveResult{
+    public static class moveResult{
         int i, j, count;
         boolean inHole;
 
-        public MoveResult(int i, int j, int count, boolean inHole){
+        public moveResult(int i, int j, int count, boolean inHole){
             this.i = i;
             this.j = j;
             this.count = count;
             this.inHole = inHole;
         }
     }
-
     public static class State{
-        int ri, rj, bi, bj, depth;
+        int ri, rj, bi, bj, time;
 
-        public State(int ri, int rj, int bi, int bj, int depth){
+        public State(int ri, int rj, int bi, int bj, int time){
             this.ri = ri;
             this.rj = rj;
             this.bi = bi;
             this.bj = bj;
-            this.depth = depth;
+            this.time = time;
         }
     }
 }

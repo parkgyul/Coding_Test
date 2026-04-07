0a35fe03fc4554b06130ab765f439846e14296a2
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-04-07 23:31:28 +0900

    [Gold I] Title: 2048 (Easy), Time: 152 ms, Memory: 17228 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -1,160 +1,136 @@
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.io.InputStreamReader;
 
 public class Main{
     static int N;
-    static int[][] arr;
     static int result = Integer.MIN_VALUE;
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        arr = new int[N][N];
+        N = Integer.parseInt(br.readLine());
 
-        for(int i =0 ; i < N; i++){
+        int[][] board = new int[N][N];
+
+        StringTokenizer st;
+        for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < N; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dfs(0, arr);
-
+        playGame(0, board);
         System.out.print(result);
     }
 
-    public static void dfs(int depth, int[][] board){
+    private static void playGame(int depth,int[][] board){
         if(depth == 5){
             result = Math.max(findMax(board), result);
             return;
         }
 
-        for(int i = 0; i < 4; i++){
-            int[][] newBoard = move(board, i);
-            dfs(depth+1, newBoard);
-        }
+        playGame(depth+1, pushLeft(board));
+        playGame(depth+1, pushRight(board));
+        playGame(depth+1, pushUp(board));
+        playGame(depth+1, pushDown(board));
     }
 
-    public static int[][] move(int[][] board, int dir){
-        int[][] next = copy(board);
-
-        switch(dir){
-            case 0: // 상
-                for(int c = 0; c < N; c++){
-                    int[] line = new int[N];
-                    int idx = 0;
-
-                    for(int r = 0; r < N; r++){
-                        if(next[r][c] != 0){
-                            line[idx++] = next[r][c];
-                        }
-                    }
-                    int[] merged = merge(line);
-
-                    for(int r = 0; r < N; r++){
-                        next[r][c] = merged[r];
-                    }
-                }
-                break;
-
-            case 1: // 하
-                for(int c = 0; c < N; c++){
-                    int[] line = new int[N];
-                    int idx = 0;
-
-                    for(int r = N-1; r >= 0; r--){
-                        if(next[r][c] != 0){
-                            line[idx++] = next[r][c];
-                        }
-                    }
-
-                    int[] merged = merge(line);
-
-                    for(int r = N-1; r >= 0; r--){
-                        next[r][c] = merged[N-r-1];
-                    }
-                }
-                break;
-
-            case 2: // 좌
-                for(int r = 0; r < N; r++){
-                    int[] line = new int[N];
-                    int idx = 0;
-
-                    for(int c = 0; c < N; c++){
-                        if(next[r][c] != 0){
-                            line[idx++] = next[r][c];
-                        }
-                    }
-
-                    int[] merged = merge(line);
-
-                    for(int c = 0; c < N; c++){
-                        next[r][c] = merged[c];
-                    }
-                }
-                break;
-            case 3: // 우
-                for(int r = 0; r < N; r++){
-                    int[] line = new int[N];
-                    int idx = 0;
-
-                    for(int c = N-1; c>= 0; c--){
-                        if(next[r][c] != 0){
-                            line[idx++] = next[r][c];
-                        }
-                    }
-
-                    int[] merged = merge(line);
-
-                    for(int c = N-1; c>= 0; c--){
-                        next[r][c] = merged[N-c-1];
-                    }
-                }
-                break;
-        }
-
-        return next;
-    }
-
-    public static int[] merge(int[] line){
-        int[] result = new int[N];
-        int idx = 0;
-
+    private static int[][] pushLeft(int[][] board){
+        int[][] newBoard = new int[N][N];
+        int idx;
         for(int i = 0; i < N; i++){
-            if(line[i] == 0) continue;
+            idx = 0;
+            for(int j = 0; j < N; j++){
+                if(board[i][j] == 0) continue;
 
-            if(i+1 < N && line[i] == line[i+1]){
-                result[idx++] = line[i] *2;
-                i += 1;
-            }else{
-                result[idx++] = line[i];
+                if(newBoard[i][idx] == 0){ //아직 값이 없을 때
+                    newBoard[i][idx] = board[i][j];
+                }else if(newBoard[i][idx] == board[i][j]){
+                    newBoard[i][idx++] *= 2;
+                }else{
+                    newBoard[i][++idx] = board[i][j];
+                }
             }
         }
 
-        return result;
+        return newBoard;
     }
 
-    public static int findMax(int[][] arr){
+    private static int[][] pushRight(int[][] board){
+        int[][] newBoard = new int[N][N];
+        int idx;
+
+        for(int i = 0; i < N; i++){
+            idx = N-1;
+            for(int j = N-1; j >= 0; j--){
+                if(board[i][j] == 0) continue;
+
+                if(newBoard[i][idx] == 0){
+                    newBoard[i][idx] = board[i][j];
+                }else if(newBoard[i][idx] == board[i][j]){
+                    newBoard[i][idx--] *= 2;
+                }else{
+                    newBoard[i][--idx] = board[i][j];
+                }
+            }
+        }
+
+        return newBoard;
+    }
+
+    private static int[][] pushUp(int[][] board){
+        int[][] newBoard = new int[N][N];
+        int idx;
+
+        for(int j = 0; j < N; j++){
+            idx = 0;
+            for(int i = 0; i < N; i++){
+                if(board[i][j] == 0) continue;
+
+                if(newBoard[idx][j] == 0){
+                    newBoard[idx][j] = board[i][j];
+                }else if(newBoard[idx][j] == board[i][j]){
+                    newBoard[idx++][j] *= 2;
+                }else{
+                    newBoard[++idx][j] = board[i][j];
+                }
+            }
+        }
+
+        return newBoard;
+    }
+
+    private static int[][] pushDown(int[][] board){
+        int[][] newBoard = new int[N][N];
+        int idx;
+
+        for(int j = 0; j < N; j++){
+            idx = N-1;
+            for(int i = N-1; i >= 0; i--){
+                if(board[i][j] == 0) continue;
+
+                if(newBoard[idx][j] == 0){
+                    newBoard[idx][j] = board[i][j];
+                }else if(newBoard[idx][j] == board[i][j]){
+                    newBoard[idx--][j] *= 2;
+                }else{
+                    newBoard[--idx][j] = board[i][j];
+                }
+            }
+        }
+
+        return newBoard;
+    }
+
+    private static int findMax(int[][] board){
         int max = Integer.MIN_VALUE;
-        for(int i =0 ; i < N; i++){
-            for(int j = 0; j < N; j++){
-                max = Math.max(max, arr[i][j]);
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board.length; j++){
+                max = Math.max(max, board[i][j]);
             }
         }
 
         return max;
-    }
-
-    public static int[][] copy(int[][] board){
-        int[][] newBoard = new int[N][N];
-
-        for(int i = 0; i < N; i++){
-            newBoard[i] = board[i].clone();
-        }
-
-        return newBoard;
     }
 }

4bb47fa73122b92bc959cd3c9162f05ed497b525
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-04-09 14:19:01 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 300 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -14,17 +14,18 @@ public class Main{
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        belt = new int[2*N];
+        belt = new int[2 * N];
         robot = new boolean[N];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < 2 * N; i ++){
+        for (int i = 0; i < 2 * N; i++) {
             belt[i] = Integer.parseInt(st.nextToken());
         }
 
         int step = 0;
+
         while(true){
-            step++;
+            step ++;
 
             rotate();
             moveRobot();
@@ -33,61 +34,50 @@ public class Main{
             if(check()) break;
         }
 
-        System.out.println(step);
+        System.out.print(step);
     }
 
     public static boolean check(){
         int cnt = 0;
-        for(int i = 0; i < 2*N; i++){
+        for(int i = 0 ; i < 2*N; i++){
             if(belt[i] == 0) cnt++;
         }
 
         return cnt >= K;
     }
 
-    public static void rotate(){
-        int last = belt[2 * N -1];
+    private static void rotate(){
+        int last = belt[2*N-1];
 
         for(int i = 2*N-1; i > 0; i--){
             belt[i] = belt[i-1];
         }
 
-        belt[0] = last;
-
         for(int i = N-1; i > 0; i--){
             robot[i] = robot[i-1];
         }
 
-        robot[0] = false;
-
+        belt[0] = last;
         robot[N-1] = false;
+        robot[0] = false;
     }
 
-    public static void moveRobot(){
-        for(int i = N-1; i>0; i--){
-            if(robot[i-1] && !robot[i] && belt[i] >= 1){
+    private static void moveRobot(){
+        for(int i = N-1; i > 0; i--){
+            if(!robot[i] && robot[i-1] && belt[i] >= 1){
                 robot[i] = true;
                 robot[i-1] = false;
-                belt[i]--;
+                belt[i] --;
             }
         }
 
         robot[N-1] = false;
     }
 
-    public static void putRobot(){
-        if(!robot[0] && belt[0] >= 1){
+    private static void putRobot(){
+        if(belt[0] >= 1){
             robot[0] = true;
             belt[0] --;
-        }
-    }
-
-    public static class Robot{
-        int i, j;
-
-        public Robot(int i, int j){
-            this.i = i;
-            this.j = j;
         }
     }
 }

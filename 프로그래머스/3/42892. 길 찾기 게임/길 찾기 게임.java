18f88dbfcde472efd70e14950ca302ba471d4047
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-14 18:18:12 +0900

    [level 3] Title: 길 찾기 게임, Time: 12.96 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,91 +1,84 @@
 import java.util.*;
 class Solution {
     static List<Node> nodes;
-    static int preIndex;
-    static int postIndex;
+    static int n;
     static int[][] answer;
+    static int index;
     public int[][] solution(int[][] nodeinfo) {
-        int n = nodeinfo.length;
+        n = nodeinfo.length;
+        answer= new int[2][n];
+        
         nodes = new ArrayList<>();
         
         for(int i = 0; i < n; i++){
+            int num = i+1;
             int x = nodeinfo[i][0];
             int y = nodeinfo[i][1];
-            int num = i+1;
-        
+            
             nodes.add(new Node(num, x, y));
         }
         
         nodes.sort((a, b) -> {
-            if(a.y == b.y) {
-                return a.x - b.x;
-            }
+           if(a.y == b.y) return a.x - b.x;
             
             return b.y - a.y;
         });
         
         Node root = nodes.get(0);
         
-        for(int i = 1; i < n; i++){
-            makeChild(root, nodes.get(i));
-        }
+       for(int i = 1; i < n; i++){
+           makeChild(root, nodes.get(i));
+       }
         
-        
-        postIndex = 0;
-        preIndex = 0;
-        answer = new int[2][n];
-        
-        postorder(root);
-        preorder(root);
+        index = 0;
+        preOrder(root);
+        index = 0;
+        postOrder(root);
         
         return answer;
     }
-    
-    private void preorder(Node node){
+    public void preOrder(Node node){
         if(node == null){
             return;
         }
         
-        answer[0][preIndex++] = node.number;
-        
-        preorder(node.left);
-        preorder(node.right);
+        answer[0][index++] = node.num;
+        preOrder(node.left);
+        preOrder(node.right);
     }
     
-    private void postorder(Node node){
+    public void postOrder(Node node){
         if(node == null){
             return;
         }
-        
-        postorder(node.left);
-        postorder(node.right);
-        
-        answer[1][postIndex++] = node.number;
+        postOrder(node.left);
+        postOrder(node.right);
+        answer[1][index++] = node.num;
     }
     
-    private void makeChild(Node parent, Node child){
-        if(parent.x > child.x){
-            if(parent.left == null){
-                parent.left = child;
-            }else{
-                makeChild(parent.left, child);
-            }
-        }else{
+    public void makeChild(Node parent, Node child){
+        if(parent.x < child.x){
             if(parent.right == null){
                 parent.right = child;
             }else{
                 makeChild(parent.right, child);
             }
+        }else{
+            if(parent.left == null){
+                parent.left = child;
+            }else{
+                makeChild(parent.left, child);
+            }
         }
     }
     
-    static class Node{
-        int number, x, y;
+    public class Node{ 
+        int num, x, y;
         Node left;
         Node right;
         
-        Node(int number, int x, int y){
-            this.number = number;
+        public Node(int num, int x, int y){
+            this.num = num;
             this.x = x;
             this.y = y;
         }

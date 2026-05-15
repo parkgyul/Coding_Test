ef076181a7d7e3d57dd4b51dc6db36e0db0ee406
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-15 14:34:43 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 32.67 ms, Memory: 220 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,47 +1,46 @@
 class Solution {
-    public int solution(int[][] board, int[][] skill) {
+    static int[][] map;
+    public int solution(int[][] board, int[][] skills) {
         int answer = 0;
+        int n = board.length;
+        int m = board[0].length;
         
-        int m = board[0].length; // 가로
-        int n = board.length; // 세로
+        map = new int[n+1][m+1];
         
-        int[][] diff = new int[n+1][m+1];
         
-        for(int i = 0; i < skill.length; i++){
-            int type = skill[i][0];
-            int r1 = skill[i][1];
-            int c1 = skill[i][2];
-            int r2 = skill[i][3];
-            int c2 = skill[i][4];
-            int degree = skill[i][5];
+        for(int[] skill : skills){
+            int type = skill[0];
+            int r1 = skill[1];
+            int c1 = skill[2];
+            int r2 = skill[3];
+            int c2 = skill[4];
+            int degree = skill[5];
             
+            degree = type == 1 ? -degree: degree;
             
-            int value = (type == 1) ? -degree : degree;
-            
-            diff[r1][c1] += value;
-            diff[r1][c2+1] -= value;
-            diff[r2+1][c1] -= value;
-            diff[r2+1][c2+1] += value;
-        }
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 1; j <m; j++){
-                diff[i][j] += diff[i][j-1];
-            }
-        }
-        
-        for(int j = 0; j < m; j++){
-            for(int i = 1; i <n; i++){
-                diff[i][j] += diff[i-1][j];
-            }
+            map[r1][c1] += degree;
+            map[r1][c2+1] -= degree;
+            map[r2+1][c1] -= degree;
+            map[r2+1][c2+1] += degree;
         }
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(board[i][j] + diff[i][j] > 0) answer++;
+                map[i][j+1] += map[i][j];
             }
         }
         
+        for(int j = 0; j < m; j++){
+            for(int i = 0; i < n; i++){
+                map[i+1][j] += map[i][j];
+            }
+        }
+    
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(map[i][j]+board[i][j] > 0) answer++;
+            }
+        }
         
         return answer;
     }

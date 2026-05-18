6f949bd593e516ec7b18d036100110ee12200123
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-18 17:36:26 +0900

    [Passed] 2N개 중에 N개의 숫자를 적절하게 고르기 (88ms, 9MB)

--- Code_Tree/trail4/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
+++ Code_Tree/trail4/2N개 중에 N개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -6,33 +6,46 @@ public class Main {
     static int[] arr;
     static int min;
     static int total;
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
 
-        arr = new int[2*N];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[2 * N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0 ; i < 2*N; i++){
+        for (int i = 0; i < 2 * N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             total += arr[i];
         }
 
         min = Integer.MAX_VALUE;
 
-        dfs(1, 0, 0);
+        // arr[0]은 무조건 선택한 그룹에 포함한다고 고정
+        dfs(1, 1, arr[0]);
 
         System.out.print(min);
     }
 
-    static void dfs(int cnt, int index, int sum){
-        if(cnt == N + 1){
-            min = Math.min(min, Math.abs(total-sum-sum));
+    static void dfs(int selectedCnt, int index, int sum) {
+        if (min == 0) {
             return;
         }
 
-        for(int i = index; i < 2*N; i++){
-            dfs(cnt+1, i+1, sum+arr[i]);
+        if (selectedCnt == N) {
+            min = Math.min(min, Math.abs(total - 2 * sum));
+            return;
+        }
+
+        int need = N - selectedCnt;
+        int remain = 2 * N - index;
+
+        if (remain < need) {
+            return;
+        }
+
+        for (int i = index; i <= 2 * N - need; i++) {
+            dfs(selectedCnt + 1, i + 1, sum + arr[i]);
         }
     }
 }

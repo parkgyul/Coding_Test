013d8e89d2a1116171a4e6dda3dc65eb9e3f32ce
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-19 16:22:35 +0900

    [Passed] 그래프 탐색 (201ms, 17MB)

---
+++ Code_Tree/trail4/그래프 탐색/graph-traversal.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static List<Integer>[] nodes;
+    static boolean[] visited;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        nodes = new ArrayList[N+1];
+        for(int i =1; i <= N; i++){
+            nodes[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+
+            nodes[n1].add(n2);
+            nodes[n2].add(n1);
+        }
+
+        visited = new boolean[N+1];
+        visited[1] = true;
+        dfs(1, 0);
+
+        int answer = -1;
+        for(boolean flag : visited){
+            if(flag) answer++;
+        }
+
+        System.out.print(answer);
+    }
+
+    static void dfs(int prev, int cnt){
+        for(int next : nodes[prev]){
+            if(visited[next]) continue;
+            visited[next] = true;
+            dfs(next, cnt+1);
+        }
+    }
+}

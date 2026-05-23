d849c5dca863554e30fe4827111ed6baeb3dda0a
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-23 22:11:46 +0900

    [Passed] 최대 증가 부분 수열 (127ms, 9MB)

---
+++ Code_Tree/trail4/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
+        }
+
+        int answer = 1;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < i; j++){
+                if(arr[j] < arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            answer = Math.max(dp[i], answer);
+        }
+
+        System.out.print(answer);
+    }
+}

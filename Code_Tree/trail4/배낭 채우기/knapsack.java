cf8aa3add1768b47088d5bed91e189179ab4ce3e
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-24 17:09:21 +0900

    [Passed] 배낭 채우기 (130ms, 13MB)

--- Code_Tree/trail4/배낭 채우기/knapsack.java
+++ Code_Tree/trail4/배낭 채우기/knapsack.java
@@ -2,43 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        final int INF = Integer.MIN_VALUE;
+        int N = Integer.parseInt(st.nextToken()); // 물건 개수
+        int M = Integer.parseInt(st.nextToken()); // 최대 무게
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[] dp = new int[M+1]; // 해당 무게 일 때 가치 최고 저장.
         int[] w = new int[N];
         int[] v = new int[N];
 
-        for(int i =0 ; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int wi = Integer.parseInt(st.nextToken());
-            int vi = Integer.parseInt(st.nextToken());
 
-            w[i] = wi;
-            v[i] = vi;
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.fill(dp, INF);
-        dp[0] = 0;
-        int answer = INF;
+        int[][] dp = new int[N + 1][M + 1];
 
-        
-        for(int j = 0; j < N; j++){
-            for(int i = M; i >= 0; i--){
-                if(i-w[j] >= 0 && dp[i-w[j]] != INF){
-                    dp[i] = Math.max(dp[i], dp[i-w[j]] + v[j]);
+        for (int i = 1; i <= N; i++) {
+            int weight = w[i - 1];
+            int value = v[i - 1];
+
+            for (int j = 0; j <= M; j++) {
+                // 1. i번째 물건을 넣지 않는 경우
+                dp[i][j] = dp[i - 1][j];
+
+                // 2. i번째 물건을 넣는 경우
+                if (j >= weight) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - weight] + value);
                 }
-                answer = Math.max(answer, dp[i]);
             }
         }
 
-        System.out.print(answer);
-
+        System.out.print(dp[N][M]);
     }
 }

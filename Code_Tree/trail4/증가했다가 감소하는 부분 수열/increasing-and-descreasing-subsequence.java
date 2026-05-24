b12087723a9d11890a11c5db77e7d0bcd6de04ed
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-24 14:50:17 +0900

    [Passed] 증가했다가 감소하는 부분 수열 (145ms, 10MB)

--- Code_Tree/trail4/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
+++ Code_Tree/trail4/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.java
@@ -2,49 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    static int n;
+    static int[] arr;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N];
-        int[] incr = new int[N];
-        int[] decr = new int[N];
-
-        Arrays.fill(incr, 1);
-        Arrays.fill(decr, 1);
-
-        int answer = 1;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        dp = new int[n][2];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i =0 ; i < N; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 1; i < N; i++){
-            for(int j = 0; j < i; j++){
-                if(arr[i] > arr[j]){
-                    incr[i] = Math.max(incr[i], incr[j] + 1);
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            // 자기 자신 하나만 선택하는 경우
+            dp[i][0] = 1; // 증가 상태
+            dp[i][1] = 1; // 감소 상태
+
+            for (int j = 0; j < i; j++) {
+
+                // 1. 증가 상태 유지
+                // 이전 값 arr[j]보다 현재 값 arr[i]가 크면 증가 수열에 붙일 수 있음
+                if (arr[j] < arr[i]) {
+                    dp[i][0] = Math.max(dp[i][0], dp[j][0] + 1);
+                }
+
+                // 2. 감소 상태 유지
+                // 이미 감소 상태인 수열 뒤에 현재 값을 붙이는 경우
+                if (arr[j] > arr[i]) {
+                    dp[i][1] = Math.max(dp[i][1], dp[j][1] + 1);
                 }
             }
 
-            answer = Math.max(answer, incr[i]);
-        }
-        
-        
+            // 3. 증가 상태에서 감소 상태로 전환
+            // i번째 원소를 꼭짓점으로 삼는 경우
+            dp[i][1] = Math.max(dp[i][1], dp[i][0]);
 
-        for(int i = N-1 ; i >= 0; i--){
-            for(int j = i+1; j < N; j++){
-                if(arr[i] > arr[j]){
-                    decr[i] = Math.max(decr[i], decr[j] + 1);
-                }
-            }
-            answer = Math.max(answer, decr[i]);
-        }
-
-        for(int i =0 ; i < N; i++){
-            answer = Math.max(answer, decr[i] + incr[i] - 1);
+            answer = Math.max(answer, dp[i][1]);
         }
 
         System.out.print(answer);

91e044165143ea8e7b1795742a78d894e8f5e335
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-27 15:40:24 +0900

    [Passed] 양수 직사각형의 최대 크기 (103ms, 9MB)

--- Code_Tree/trail4/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ Code_Tree/trail4/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -24,32 +24,28 @@ public class Main {
 
         int answer = -1;
 
-        for (int top = 0; top < N; top++) {
-            boolean[] possible = new boolean[M];
-            Arrays.fill(possible, true);
-
-            for (int bottom = top; bottom < N; bottom++) {
-
-                for (int col = 0; col < M; col++) {
-                    if (arr[bottom][col] <= 0) {
-                        possible[col] = false;
-                    }
-                }
-
-                int height = bottom - top + 1;
-                int width = 0;
-
-                for (int col = 0; col < M; col++) {
-                    if (possible[col]) {
-                        width++;
-                        answer = Math.max(answer, height * width);
-                    } else {
-                        width = 0;
+        for(int i =0 ; i < N; i++){
+            for(int j = 0 ; j < M; j++){
+                for(int k = i ; k < N; k++){
+                    for(int l = j; l < M; l++){
+                        if(isPositive(i, j, k, l)){
+                            answer = Math.max(answer, (k-i+1)*(l-j+1));
+                        }
                     }
                 }
             }
         }
 
         System.out.println(answer);
+    }
+
+    static boolean isPositive(int x1, int y1, int x2, int y2){
+        for(int i = x1; i <= x2; i++){
+            for(int j = y1; j <= y2; j++){
+                if(arr[i][j] <= 0) return false;
+            }
+        }
+
+        return true;
     }
 }

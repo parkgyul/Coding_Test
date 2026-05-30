e91bc2fd83c2cde2ee92d7f5e473add035478f3d
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-30 22:00:09 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,6 +1,9 @@
-Select sum(g.score) as score, e.emp_no, e.emp_name, e.position, e.email
-from HR_EMPLOYEES e join HR_GRADE g on e.emp_no = g.emp_no
-where g.year = 2022
-group by e.emp_no
-order by score desc
-limit 1;
+select t.total_score as score, e.EMP_NO, e.EMP_NAME, e.POSITION, e.EMAIL
+from HR_EMPLOYEES e 
+join(
+    select h.emp_no, sum(g.score) total_score
+    from HR_EMPLOYEES h join HR_GRADE g on h.EMP_NO = g.EMP_NO
+    group by h.emp_no
+    order by total_score desc
+    limit 1
+)t on e.emp_no = t.emp_no;

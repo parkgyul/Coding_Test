974158c8510ec0e62787d717ad5ca6c830b10101
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-30 21:19:24 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,15 +1,13 @@
--- 코드를 입력하세요
-SELECT YEAR(o.SALES_DATE) as YEAR, 
-        MONTH(o.SALES_DATE) as MONTH, 
-        count(DISTINCT o.user_id) as PURCHASED_USERS, 
-        ROUND(count( DISTINCT o.user_id) /
-         (
-            select count(*)
-             from USER_INFO w
-             where year(w.joined) = 2021
-         ), 1)
-         as PUCHASED_RATIO
-from USER_INFO i join ONLINE_SALE o on i.USER_ID = o.USER_ID                
-where YEAR(i.joined) = 2021                       
-group by YEAR(o.SALES_DATE), MONTH(o.SALES_DATE)
-order by YEAR(o.SALES_DATE), MONTH(o.SALES_DATE);
+select YEAR(s.SALES_DATE) as year, month(s.SALES_DATE) as month,
+     count(distinct i.user_id) as PURCHASED_USERS,
+    ROUND(
+        count(distinct i.user_id) / (
+            select count(distinct(w.user_id))
+            from USER_INFO w
+            where year(w.joined) = 2021
+        ), 1
+    ) as PUCHASED_RATIO
+from USER_INFO i join ONLINE_SALE s on i.user_id = s.user_id
+where YEAR(i.joined) = 2021
+group by year(s.sales_date), month(s.SALES_DATE)
+order by year(s.sales_date), month(s.SALES_DATE);

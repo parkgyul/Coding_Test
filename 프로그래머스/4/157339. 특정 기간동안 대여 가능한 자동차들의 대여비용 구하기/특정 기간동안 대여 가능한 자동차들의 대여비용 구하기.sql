315c2a606b0b09420eb9acd06b96800fc66eb019
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-05-30 20:24:10 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,22 +1,15 @@
-SELECT 
-    c.CAR_ID, 
-    c.CAR_TYPE, 
-    ROUND(c.DAILY_FEE * 30 * (100 - p.DISCOUNT_RATE) / 100) AS FEE
-FROM CAR_RENTAL_COMPANY_CAR c
-JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN p 
-    ON c.CAR_TYPE = p.CAR_TYPE
-WHERE c.CAR_TYPE IN ('세단', 'SUV') 
-  AND p.DURATION_TYPE = '30일 이상'
-  AND NOT EXISTS (
-      SELECT 1
-      FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY h
-      WHERE c.CAR_ID = h.CAR_ID
-        AND h.START_DATE <= '2022-11-30'
-        AND h.END_DATE >= '2022-11-01'
-  )
-  AND ROUND(c.DAILY_FEE * 30 * (100 - p.DISCOUNT_RATE) / 100) >= 500000
-  AND ROUND(c.DAILY_FEE * 30 * (100 - p.DISCOUNT_RATE) / 100) < 2000000
-ORDER BY 
-    FEE DESC,
-    c.CAR_TYPE ASC,
-    c.CAR_ID DESC;
+select c.CAR_ID, c.CAR_TYPE, ROUND((c.DAILY_FEE * 30 *(100-p.DISCOUNT_RATE) / 100)) as fee
+from CAR_RENTAL_COMPANY_CAR c join CAR_RENTAL_COMPANY_DISCOUNT_PLAN p 
+    on c.car_type = p.car_type
+where c.CAR_TYPE in ('세단', 'SUV')
+       AND p.DURATION_TYPE = '30일 이상'
+       AND NOT EXISTS (
+          SELECT 1
+          FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY h
+          WHERE c.CAR_ID = h.CAR_ID
+            AND h.START_DATE <= '2022-11-30'
+            AND h.END_DATE >= '2022-11-01'
+      )
+      and ROUND((c.DAILY_FEE * 30 *(100-p.DISCOUNT_RATE) / 100)) >= 500000
+      and ROUND((c.DAILY_FEE * 30 *(100-p.DISCOUNT_RATE) / 100)) < 2000000
+order by fee desc,  c.CAR_TYPE,  c.CAR_ID ASC;

3f7c8b00b51192633a4bf81d14bc55dd2cd27e2c
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-07-16 13:48:38 +0900

    [Passed] 고대 보물 지도의 비밀 (99ms, 9MB)

--- Code_Tree/trail4/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
+++ Code_Tree/trail4/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static final int INF = 1000000000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,28 +17,42 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        final int NEG_INF = Integer.MIN_VALUE / 2; // 덧셈 오버플로 방지용
-        int[][] dp = new int[N + 1][K + 1];
-        for (int[] row : dp) Arrays.fill(row, NEG_INF);
+        int[][] dp = new int[N+1][K+1];
 
-        int answer = Integer.MIN_VALUE;
+        for(int i = 0; i <= N; i++){
+            Arrays.fill(dp[i], -INF);
+        }
 
-        for (int i = 1; i <= N; i++) {
-            for (int cnt = 0; cnt <= K; cnt++) {
-                if (arr[i] < 0) {
-                    if (cnt >= 1) {
-                        int prev = dp[i - 1][cnt - 1]; // 이전 칸에서 이어붙이는 경우
-                        dp[i][cnt] = Math.max(arr[i], prev + arr[i]);
-                    }
-                    // cnt == 0이면 음수 원소를 포함할 수 없으므로 NEG_INF 그대로 둠
-                } else {
-                    int prev = dp[i - 1][cnt];
-                    dp[i][cnt] = Math.max(arr[i], prev + arr[i]);
+        if(arr[1] >= 0){
+            dp[1][0] = arr[1];
+        }else{
+            dp[1][1] = arr[1];
+        }
+
+        int answer = arr[1];
+
+        for(int i = 2; i <= N; i++){
+            if(arr[i] >= 0){ //양수 일떄
+                dp[i][0] = Math.max(dp[i-1][0] + arr[i], arr[i]);
+                answer = Math.max(answer, dp[i][0]);
+
+                for(int j = 1; j <= K; j++){
+                    if(dp[i-1][j] == -INF ) continue;
+                    dp[i][j] = dp[i-1][j] + arr[i];
+                    answer = Math.max(answer, dp[i][j]);
                 }
+            }else{
+                dp[i][1] = Math.max(dp[i-1][0] + arr[i], arr[i]);
+                answer = Math.max(answer, dp[i][1]);
 
-                answer = Math.max(answer, dp[i][cnt]);
+                for(int j = 2; j <= K; j++){
+                    if(dp[i-1][j-1] == -INF) continue;
+                    dp[i][j] = dp[i-1][j-1] + arr[i];
+                    answer = Math.max(answer, dp[i][j]);
+                }
             }
         }
+
 
         System.out.print(answer);
     }

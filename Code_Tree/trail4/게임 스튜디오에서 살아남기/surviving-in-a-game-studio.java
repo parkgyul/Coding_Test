0b3222a0a4f82cec4302aab3686a11b63f112325
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-07-16 22:07:28 +0900

    [Passed] 게임 스튜디오에서 살아남기 (105ms, 9MB)

---
+++ Code_Tree/trail4/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final long OFFSET = (long)Math.pow(10, 9) + 7;
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        long[][][] dp = new long[N+1][3][3];
+
+        dp[0][0][0] = 1;
+
+        long answer = 0;
+
+        for(int i = 1 ; i <= N; i++){
+            for(int j = 0; j < 3; j++){
+                for(int k = 0; k < 3; k++){
+                    long prev = dp[i-1][j][k];
+                    if(prev == 0) continue;
+
+                    // G
+                    dp[i][0][k] = (dp[i][0][k] + prev) % OFFSET;
+
+                    // B 선택
+                    if(j + 1 < 3){
+                        dp[i][j+1][k] = (dp[i][j+1][k] + prev) % OFFSET;
+                    }
+
+                    // T 선택
+                    if(k + 1 < 3){ 
+                        dp[i][0][k+1] = (dp[i][0][k+1] + prev) % OFFSET;
+                    }
+                }
+            }
+        }
+
+         for(int j = 0; j < 3; j++){
+                for(int k = 0; k < 3; k++){
+                    answer += dp[N][j][k];
+                }
+         }
+
+         System.out.print(answer % OFFSET);
+    }
+}

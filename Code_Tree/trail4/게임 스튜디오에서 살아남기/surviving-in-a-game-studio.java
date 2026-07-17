c1f7bde4f43f0df9097e7dac499ea481fb8c29e3
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-07-17 14:41:56 +0900

    [Passed] 게임 스튜디오에서 살아남기 (95ms, 9MB)

--- Code_Tree/trail4/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.java
+++ Code_Tree/trail4/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.java
@@ -15,33 +15,30 @@ public class Main {
 
         long answer = 0;
 
-        for(int i = 1 ; i <= N; i++){
+        for(int i = 1; i <= N; i++){
             for(int j = 0; j < 3; j++){
                 for(int k = 0; k < 3; k++){
-                    long prev = dp[i-1][j][k];
-                    if(prev == 0) continue;
 
-                    // G
-                    dp[i][0][k] = (dp[i][0][k] + prev) % OFFSET;
+                    // G 선택
+                    dp[i][j][0] =  (dp[i][j][0] + dp[i-1][j][k]) % OFFSET;
 
-                    // B 선택
-                    if(j + 1 < 3){
-                        dp[i][j+1][k] = (dp[i][j+1][k] + prev) % OFFSET;
+                    if(k < 2){
+                        dp[i][j][k+1] = (dp[i][j][k+1] + dp[i-1][j][k]) % OFFSET;
                     }
 
-                    // T 선택
-                    if(k + 1 < 3){ 
-                        dp[i][0][k+1] = (dp[i][0][k+1] + prev) % OFFSET;
+                    if(j < 2){
+                        dp[i][j+1][0] = (dp[i][j+1][0] + dp[i-1][j][k]) % OFFSET;
                     }
+                    
                 }
             }
         }
 
-         for(int j = 0; j < 3; j++){
-                for(int k = 0; k < 3; k++){
-                    answer += dp[N][j][k];
-                }
-         }
+        for(int j = 0; j < 3; j++){
+            for(int k = 0; k < 3; k++){
+                answer += (dp[N][j][k]%OFFSET);
+            }
+        }
 
          System.out.print(answer % OFFSET);
     }

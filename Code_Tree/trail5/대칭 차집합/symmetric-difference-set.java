fd02f145ce3c82239a303c610ab75e43850da591
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-07-21 13:51:34 +0900

    [Passed] 대칭 차집합 (578ms, 43MB)

--- Code_Tree/trail5/대칭 차집합/symmetric-difference-set.java
+++ Code_Tree/trail5/대칭 차집합/symmetric-difference-set.java
@@ -19,18 +19,14 @@ public class Main {
 
             set.add(num);
         }
-
+        int cnt =0;
         
         st = new StringTokenizer(br.readLine());
         for(int j = 0; j < M; j++){
             int num = Integer.parseInt(st.nextToken());
-            if(set.contains(num)){
-                set.remove(num);
-            }else{
-                set.add(num);
-            }
+            if(set.contains(num)) cnt += 2;
         }
 
-        System.out.print(set.size());
+        System.out.print(N+M-cnt);
     }
 }

b864f650482edb6d39ab5162399a84c8490987c3
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-07-22 23:59:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.83 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String str : participant){
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        
+        for(String str : completion){
+            if(map.get(str) == 1){
+                map.remove(str);
+            }else{
+                map.put(str, map.get(str)-1);
+            }
+        }
+        
+        for(String str : map.keySet()){
+            answer = str;
+        }
+        
+        return answer;
+    }
+}

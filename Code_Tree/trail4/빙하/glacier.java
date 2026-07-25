ca5e9dd8d934bfe97b8ae2004b25fa77c649a809
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-07-25 14:53:47 +0900

    [Passed] 빙하 (96ms, 9MB)

--- Code_Tree/trail4/빙하/glacier.java
+++ Code_Tree/trail4/빙하/glacier.java
@@ -1,73 +1,75 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
 
-        int[][] map = new int[n][m];
-        int ice = 0;
-        for(int i = 0; i < n; i++) {
+        int rest = 0;
+
+        for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            for(int j = 0; j < M; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 1) ice++;
+
+                if(map[i][j] == 1) rest++;
             }
         }
-        
-        boolean[][] visited = new boolean[n][m];
-        Queue<Point> waters = new ArrayDeque<>();
-        
-        waters.add(new Point(0, 0));
-        visited[0][0] = true;
-
         int time = 0;
-        int lastMelted = 0;
+        visited = new boolean[N][M];
+        visited[0][0] = true;
+        Queue<Point> melted = new LinkedList<>();
+        melted.add(new Point(0, 0));
 
-        while(ice > 0) {
-            int meltedCount = 0;
-            Queue<Point> melted = new ArrayDeque<>();
+        Queue<Point> melting = bfs(melted);
+        int lastSize = melting.size();
 
-            while(!waters.isEmpty()) {
-                Point cur = waters.poll();
+        while(!melting.isEmpty()){
+            time ++ ;
 
-                for (int i = 0; i < 4; i++) {
-                    int ny = cur.i + dy[i];
-                    int nx = cur.j + dx[i];
-
-                    if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-                    if (visited[ny][nx]) continue;
-                    
-                    visited[ny][nx] = true;
-                    
-                    if (map[ny][nx] == 1) {
-                        meltedCount++;
-                        map[ny][nx] = 0;
-                        melted.add(new Point(ny, nx));
-                        ice--;
-                    } else {
-                        waters.add(new Point(ny, nx));
-                    }
-                }
-            }
-            
-            lastMelted = meltedCount;
-            time++;
-
-            waters = melted;
+            melting = bfs(melting);
+            if(!melting.isEmpty()) lastSize = melting.size();
         }
-        System.out.println(time + " " + lastMelted);
+
+        System.out.print(time + " " + lastSize);
+
+    }
+
+    static Queue<Point> bfs(Queue<Point> q){
+        Queue<Point> newMelting = new LinkedList<>();
+
+        while(!q.isEmpty()){
+            Point cur = q.poll();
+
+            for(int i =0 ; i < 4; i++){
+                int ni = cur.i + dx[i];
+                int nj = cur.j + dy[i];
+
+                if(ni < 0 || ni >= N || nj < 0 || nj >= M) continue;
+                if(visited[ni][nj]) continue;
+
+                visited[ni][nj] = true;
+                if(map[ni][nj] == 0) q.add(new Point(ni, nj));
+                else newMelting.add(new Point(ni, nj));
+            }
+        }
+        return newMelting;
     }
 
     static class Point{
         int i, j;
+
         Point(int i, int j){
             this.i = i;
             this.j = j;

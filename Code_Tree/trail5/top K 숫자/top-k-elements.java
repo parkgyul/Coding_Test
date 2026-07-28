c26a1814d2afd92b72b07234c1de6295bcf80cde
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-07-28 10:47:22 +0900

    [Passed] top K 숫자 (346ms, 22MB)

--- Code_Tree/trail5/top K 숫자/top-k-elements.java
+++ Code_Tree/trail5/top K 숫자/top-k-elements.java
@@ -1,33 +1,36 @@
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.io.*; 
+import java.util.*;
 
 public class Main {
-    // 변수 선언
-    public static int n, k;
-    public static TreeSet<Integer> s = new TreeSet<>();
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-        k = sc.nextInt();
-        
-        // 입력받은 수들을 treeset에 넣어줍니다.
-        for(int i = 0; i < n; i++) {
-            int x = sc.nextInt();
-            s.add(-x);
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+    
+        TreeSet<Integer> set = new TreeSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            set.add(-Integer.parseInt(st.nextToken()));
         }
 
-        // cnt : 출력한 숫자의 개수
         int cnt = 0;
 
         // iterator를 이용해 가장 큰 k개의 숫자를 출력합니다.
-        for(int num : s) {
-            System.out.print(-num + " ");
+        for(int num : set) {
+            sb.append(-num + " ");
             cnt++;
 
-            if(cnt == k)
+            if(cnt == K)
                 break;
         }
+
+        System.out.print(sb);
+        
     }
 }

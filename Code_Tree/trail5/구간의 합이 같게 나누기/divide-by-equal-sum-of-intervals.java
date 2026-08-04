3cb1fd0624932568ec58e532d9e74ef3099e306b
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-08-04 17:39:51 +0900

    [Passed] 구간의 합이 같게 나누기 (524ms, 18MB)

---
+++ Code_Tree/trail5/구간의 합이 같게 나누기/divide-by-equal-sum-of-intervals.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        int[] arr = new int[N];
+        long[] L = new long[N];
+        long[] R = new long[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        L[0] = (long) arr[0];
+        R[N-1] = (long) arr[N-1];
+        for(int i = 1 ; i < N; i++){
+            L[i] = L[i-1] + (long)arr[i];
+        }
+
+        for(int i = N-2; i >= 0; i--){
+            R[i] = R[i+1] + (long)arr[i];
+        }
+
+        long answer =0;
+
+        for(int k = 1; k <= N-3; k++){
+            if(L[k] != R[k+1]) continue;
+            int left = 0;
+            int right = 0;
+            for(int i = 0; i <= k-1; i++){
+                if(2*L[i] == L[k]) left++;
+            }
+
+            for(int j = k+2; j <= N-1; j++){
+                if(2*R[j] == R[k+1]) right++;
+            }
+
+            answer +=(left*right);
+        }
+
+        System.out.print(answer);
+    }
+}

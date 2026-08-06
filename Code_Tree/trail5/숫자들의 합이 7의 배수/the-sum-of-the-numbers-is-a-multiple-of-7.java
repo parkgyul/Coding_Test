7004e88d2b59793c28d85c27b3532794c8e972ae
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-08-06 21:37:35 +0900

    [Passed] 숫자들의 합이 7의 배수 (89ms, 9MB)

--- Code_Tree/trail5/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
+++ Code_Tree/trail5/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.java
@@ -2,34 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+
         int N = Integer.parseInt(br.readLine());
+        int answer = 0;
 
-        int[] sum = new int[N];
-        for(int i = 0; i < N; i++){
-            int num = Integer.parseInt(br.readLine()) % 7;
-            sum[i] += (i == 0 ? num : sum[i-1] + num);
+        long[] sums = new long[N + 1];
+        int[] first = new int[7];
+
+        Arrays.fill(first, -1);
+        first[0] = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sums[i] = sums[i - 1] + Integer.parseInt(br.readLine());
+
+            int occur = (int) (sums[i] % 7);
+
+            if(first[occur] != -1) answer = Math.max(answer, i - first[occur]);
+            else first[occur] = i;
         }
 
-        int max = 0;
-        for(int i = N-1; i >= 0; i--){
-            if(sum[i] % 7 == 0){
-                max = Math.max(max, i+1);
-                continue;
-            }
-
-            for(int j = 0; j <= i-1; j++){
-                if(i - j <= max) break;
-
-                if((sum[i] - sum[j]) % 7== 0){
-                    max = Math.max(max, i-j);
-                    break;
-                }
-            }
-        }
-
-        System.out.print(max);
+        System.out.print(answer);
     }
 }

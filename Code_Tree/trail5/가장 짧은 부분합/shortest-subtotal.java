ad663694b59c04d6031e37989f74594a01e21a68
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-08-07 11:59:34 +0900

    [Passed] 가장 짧은 부분합 (175ms, 13MB)

---
+++ Code_Tree/trail5/가장 짧은 부분합/shortest-subtotal.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = Integer.MAX_VALUE;
+        long sum = 0;
+        int j = 0;
+        for(int i = 1; i <= N; i++){
+            while(j+1 <= N && sum + arr[j+1] < K){
+                sum += arr[j+1];
+                j++;
+            }
+
+            if(j+1 <= N && sum + arr[j + 1] >= K){
+                min = Math.min(min, j-i+2);
+            }
+
+            sum -= arr[i];
+        }
+
+        System.out.print(min == Integer.MAX_VALUE ? -1 : min);
+    }
+}

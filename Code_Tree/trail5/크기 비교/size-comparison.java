b50788c2f39b5001a9f4dbcbf161c064c59963b0
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-08-19 20:28:10 +0900

    [Passed] 크기 비교 (141ms, 10MB)

--- Code_Tree/trail5/크기 비교/size-comparison.java
+++ Code_Tree/trail5/크기 비교/size-comparison.java
@@ -3,15 +3,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args)throws IOException {
-        final int INF = 10000000;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         int[][] com = new int[N+1][N+1];
-        int[][] com2 = new int[N+1][N+1];
 
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
@@ -19,7 +16,6 @@ public class Main {
             int to = Integer.parseInt(st.nextToken());
 
             com[fr][to] = 1;
-            com2[to][fr] = 1;
         }
 
         StringBuilder sb = new StringBuilder();
@@ -30,10 +26,6 @@ public class Main {
                     if(com[i][k] ==1 && com[k][j] == 1){
                         com[i][j] = 1;
                     }
-
-                    if(com2[i][k] == 1 && com2[k][j] == 1){
-                        com2[i][j]= 1; 
-                    }
                 }
             }
         }
@@ -41,10 +33,13 @@ public class Main {
         for(int i = 1; i <= N; i++){
             int cnt = 0; 
             for(int j = 1; j <= N ; j++){
-                cnt += (com[i][j] + com2[i][j]);
+                if(i == j) continue;
+                if(com[i][j] == 1 || com[j][i] == 1) continue;
+
+                cnt++;
             }
 
-            sb.append(N-1-cnt).append("\n");
+            sb.append(cnt).append("\n");
         }
 
         System.out.print(sb);

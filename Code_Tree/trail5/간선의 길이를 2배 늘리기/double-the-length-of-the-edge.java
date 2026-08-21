cd23eafcf8ebb409ddb57f81635f8b806415dd83
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-08-21 21:34:26 +0900

    [Passed] 간선의 길이를 2배 늘리기 (615ms, 20MB)

---
+++ Code_Tree/trail5/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.*; 
+
+public class Main {
+    static int N, M;
+    static int[][] distance;
+    static final int INF = Integer.MAX_VALUE;
+
+    static int[] storeFrom, storeTo;
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        distance = new int[N+1][N+1];
+
+        for(int i = 1; i <= N; i++){
+            Arrays.fill(distance[i], INF);
+        }
+
+        storeFrom = new int[M];
+        storeTo = new int[M];
+    
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int fr = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int co = Integer.parseInt(st.nextToken());
+
+            distance[fr][to] = co;
+            distance[to][fr] = co;
+
+            storeFrom[i] = fr;
+            storeTo[i] = to;
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        int first = dijkstra();
+
+        for(int i = 0; i < M; i++){
+            int fr = storeFrom[i];
+            int to = storeTo[i];
+
+            distance[fr][to] *= 2;
+            distance[to][fr] *= 2;
+
+            int value = dijkstra();
+
+            max = Math.max(max, value-first);
+
+            distance[fr][to] /= 2;
+            distance[to][fr] /= 2;
+        }
+        
+
+        System.out.print(max);
+        
+    }
+
+    static int dijkstra(){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(1, 0));
+
+        int[] costs = new int[N+1];
+
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        costs[1] = 0;
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+
+            if(costs[cur.node] < cur.cost) continue;
+
+            for(int next = 1; next <= N; next++){
+                if(cur.node == next) continue;
+
+                if(distance[cur.node][next] == INF) continue;
+
+                int nextCost = distance[cur.node][next] + cur.cost;
+
+                if(nextCost >= costs[next]) continue;
+
+                costs[next] = nextCost;
+                pq.add(new Node(next, nextCost));
+            }
+        }
+
+        return costs[N];
+    }
+
+
+    static class Node implements Comparable<Node>{
+        int node, cost;
+        Node(int node, int cost){
+            this.node = node;
+            this.cost = cost;
+        }
+
+        public int compareTo(Node o){
+            return this.cost - o.cost;
+        } 
+    }
+}

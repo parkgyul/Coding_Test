fa3b019993bb8166528c8b84f6cc735da320745e
Gyuri <98478441+parkgyul@users.noreply.github.com>
2026-09-12 18:46:22 +0900

    [LeetCode] 0091-decode-ways : 09-12-2026

---
+++ LeetCode/Medium/0091-decode-ways/0091-decode-ways-09-12-2026-18-46-22.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length()+1];
+
+        if(s.charAt(0) == '0') return 0;
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= s.length(); i++){
+            int oneDigit = s.charAt(i-1) - '0';
+            if(oneDigit >= 1 && oneDigit <= 9){
+                dp[i] += dp[i-1];
+            }
+
+            int twoDigit = Integer.parseInt(s.substring(i-2, i));
+            if(twoDigit >= 10 && twoDigit <= 26){
+                dp[i] += dp[i-2];
+            }
+        }
+        
+        return dp[s.length()];
+    }
+}
